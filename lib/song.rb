c3e3fa9f2523421713e0e68ae92fbec7cf1b24c3
Anna Dybas <a.m.dybas@gmail.com>
2019-06-04 03:28:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    split_name = filename(" - ")
+    split_name = filename.split(" - ")
     new_song = self.new(split_name[1])
     new_artist = Artist.new(split_name[0])
   end
