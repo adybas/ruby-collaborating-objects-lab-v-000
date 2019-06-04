cfb2bb1e809e321a1c17d7931e4f1680964e3e77
Anna Dybas <a.m.dybas@gmail.com>
2019-06-04 03:38:09 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,16 +1,15 @@
 class MP3Importer
-  attr_accessor :path
+  attr_reader :path
 
-  def initialize(filename)
-    @path = filename
+  def initialize(path)
+    @path = path
   end
 
   def files
-    @files = Dir.entries(@path)
-    @files.delete_if{|element| element == "." || element == ".."}
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
 
   def import
-    files.each {|file|Song.new_by_filename(file)}
+    files.each{|f| Song.new_by_filename(f)}
   end
 end
